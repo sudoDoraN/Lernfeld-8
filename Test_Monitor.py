a09134ea621aad71a037d887dc5abd8b5ec35638
HonorTheHaze <habe.keine.email100@gmail.com>
2022-12-13 13:26:17 +0100

    Update: Unittest

--- Test_Monitor.py
+++ Test_Monitor.py
@@ -1,16 +1,28 @@
 import unittest
 import MonitorRealtime
+import pathlib
+import datetime
 
 class TestMonitor(unittest.TestCase):
 
+    def test_osname(self):
+        self.assertEqual("Posix", MonitorRealtime.GetOSName())
+
     def test_system(self):
-        self.assertEqual("NT", MonitorRealtime.GetSystem())
+        self.assertEqual("Linux", MonitorRealtime.GetSystem())
+
+    def test_cpu_count(self):
+        self.assertEqual(2, MonitorRealtime.GetCPUCount(False))
 
     def test_release(self):
-        self.assertEqual("10/11", MonitorRealtime.GetRelease())
+        self.assertEqual("5.15.0-1024-azure", MonitorRealtime.GetRelease())
 
-    def test_hostname(self):
-        self.assertEqual("Fynn-PC", MonitorRealtime.GetHostname())
+    def test_user(self):
+        self.assertEqual("runner", MonitorRealtime.GetLoggedInUser())
+
+    def text_exists_log(self):
+        if not pathlib.Path(f"{datetime.today().strftime('%d-%m-%y')}.log").resolve().is_file():
+            raise AssertionError("Log-File existiert nicht!")
 
 if __name__ == '__main__':
     unittest.main()
